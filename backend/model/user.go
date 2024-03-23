36aae438f3dd9d2891631a73fabd514e5c71112e
sungyo4869 <hchengjian1@gmail.com>
2024-03-23 16:14:54 +0900

    userに関連する構造体を定義した

--- backend/model/user.go
+++ backend/model/user.go
@@ -7,4 +7,39 @@ type (
 		Password	string	`json:"password"`
 		Email		string	`json:"email"`
 	}
+
+	CreateUserRequest struct{
+		UserName	string	`json:"username"`
+		Password	string	`json:"password"`
+		Email		string	`json:"email"`
+	}
+
+	CreateUserResponse struct{
+		USER	USER	`json:"user"`
+	}
+
+	ReadUserRequest struct {
+		UserName	string	`json:"username"`
+	}
+
+	ReadUserResponse struct {
+		USER	USER	`json:"user"`
+	}
+
+	UpdateUserRequest struct {
+		UserName	string	`json:"username"`
+		Password	string	`json:"password"`
+		Email		string	`json:"email"`
+	}
+
+	UpdateUserResponse	struct {
+		USER	USER	`json:"user"`
+	}
+
+	DeleteUserRequest struct {
+		UserName	string	`json:"username"`
+		Password	string	`json:"password"`
+	}
+
+	DeleteUserResponse struct {}
 )
