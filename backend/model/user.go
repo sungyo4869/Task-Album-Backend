5e2742e64beae38e75f197424ee8441821e728eb
sungyo4869 <hchengjian1@gmail.com>
2024-03-24 03:25:31 +0900

    fmtを整えた

--- backend/model/user.go
+++ backend/model/user.go
@@ -1,45 +1,45 @@
 package model
 
 type (
-	USER struct{
-		ID			int64	`json:"id"`
-		UserName	string	`json:"username"`
-		Password	string	`json:"password"`
-		Email		string	`json:"email"`
+	USER struct {
+		ID       int64  `json:"id"`
+		UserName string `json:"username"`
+		Password string `json:"password"`
+		Email    string `json:"email"`
 	}
 
-	CreateUserRequest struct{
-		UserName	string	`json:"username"`
-		Password	string	`json:"password"`
-		Email		string	`json:"email"`
+	CreateUserRequest struct {
+		UserName string `json:"username"`
+		Password string `json:"password"`
+		Email    string `json:"email"`
 	}
 
-	CreateUserResponse struct{
-		USER	USER	`json:"user"`
+	CreateUserResponse struct {
+		User USER `json:"user"`
 	}
 
 	ReadUserRequest struct {
-		UserName	string	`json:"username"`
+		UserName string `json:"username"`
 	}
 
 	ReadUserResponse struct {
-		USER	USER	`json:"user"`
+		User USER `json:"user"`
 	}
 
 	UpdateUserRequest struct {
-		UserName	string	`json:"username"`
-		Password	string	`json:"password"`
-		Email		string	`json:"email"`
+		UserName string `json:"username"`
+		Password string `json:"password"`
+		Email    string `json:"email"`
 	}
 
-	UpdateUserResponse	struct {
-		USER	USER	`json:"user"`
+	UpdateUserResponse struct {
+		User USER `json:"user"`
 	}
 
 	DeleteUserRequest struct {
-		UserName	string	`json:"username"`
-		Password	string	`json:"password"`
+		UserName string `json:"username"`
+		Password string `json:"password"`
 	}
 
-	DeleteUserResponse struct {}
+	DeleteUserResponse struct{}
 )
