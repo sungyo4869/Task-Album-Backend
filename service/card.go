7fde84b49add4ad4a9a42d9356b29fe4b9f18335
sungyo4869 <hchengjian1@gmail.com>
2024-04-04 04:52:14 +0900

    commitし忘れていたファイルのcommit

---
+++ service/card.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/sungyo4869/portfolio/model"
+)
+
+type CardService struct {
+	db *sql.DB
+}
+
+func NewCardService(db *sql.DB) *CardService {
+	return &CardService{
+		db: db,
+	}
+}
+
+func (s *CardService) CreateCard(ctx context.Context, title, summary, time_limit, status, description string) (*model.Card, error) {
+
+	var card model.Card
+
+	return &card, nil
+}
+func (s *CardService) ReadCard(ctx context.Context) ([]*model.Card, error) {
+	var cards []*model.Card
+
+	return cards, nil
+}
+func (s *CardService) UpdateCard(ctx context.Context) (*model.Card, error) {
+	var card model.Card
+
+	return &card, nil
+}
+func (s *CardService) DeleteCard(ctx context.Context) error {
+	return nil
+}
