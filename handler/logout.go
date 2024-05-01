a07b793fd06d62b5b6201b4042671985ad269b02
sungyo4869 <hchengjian1@gmail.com>
2024-05-01 11:17:33 +0900

    要らなそうなメソッド消した

--- handler/logout.go
+++ handler/logout.go
@@ -10,9 +10,7 @@ func NewLogoutHandler() *LogoutHandler {
 
 func (h *LogoutHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
-	case http.MethodPost:
-	case http.MethodPut:
 	case http.MethodGet:
-	case http.MethodDelete:
+		
 	}
 }
